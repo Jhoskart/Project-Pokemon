8a218e0b3534476e7a0bdb13b3e6443b8ae2365b
Jhoskar Toro <jhoskartoro@gmail.com>
2022-10-24 11:46:13 -0300

    new change

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, PORT
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 let sequelize = 
@@ -12,7 +12,7 @@ let sequelize =
     database: DB_NAME,
     dialect: 'postgres',
     host: DB_HOST,
-    port: PORT,
+    port: DB_PORT,
     username: DB_USER,
     password: DB_PASSWORD,
     pool: {
